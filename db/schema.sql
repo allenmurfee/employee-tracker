e6dcf17e2b4169d760dfeb23427b9b8bcb6ad4b9
allenmurfee <allenmurfee123@gmail.com>
2022-11-07 12:43:42 -0500

    worked on inquirer and schema sql file

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ create table role (
 id INT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INT
+department_id INT,
+foreign key (department_id)
+references department (id)
 );
 
 create table employee (
@@ -20,5 +22,9 @@ id INT PRIMARY KEY,
 first_name VARCHAR(30), 
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT 
+manager_id INT,
+foreign key (role_id)
+references role(id),
+foreign key (manager_id)
+references employee(id)
 );
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const express = require("express");
-const questions = require("./questions");
+const prompts = require("./questions");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +9,40 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password here
+    password: "rootroot",
+    database: "company_db",
+  },
+  console.log(`Connected to the company_db database.`)
+);
+
 const start = (questions) => {
-  inquirer.prompt(questions).then((answers) => {});
+  inquirer.prompt(questions).then((answers) => {
+    if (answers.next === "Add a department") {
+      db.query("SELECT * FROM students", function (err, results) {
+        console.log(results);
+      });
+      //Need to push to array
+      start(prompts.addDepartment);
+    } else if (answers.next === "Add a role") {
+      db.query("SELECT * FROM students", function (err, results) {
+        console.log(results);
+      });
+      //Need to push to array
+      start(prompts.addRole);
+    } else if (answers.next === "add an employee") {
+      db.query("SELECT * FROM students", function (err, results) {
+        console.log(results);
+      });
+      //Need to push to array
+      start(prompts.addEmployee);
+    }
+  });
 };
 
-start();
+start(prompts.introQuestion);
