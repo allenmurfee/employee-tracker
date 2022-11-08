2bcebb699cf682c44ab4f19ad22bbe2cf4e0e602
allenmurfee <allenmurfee123@gmail.com>
2022-11-08 12:29:24 -0500

    Worked on SQL

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ create database company_db;
 use company_db;
 
 create table department (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30) 
 );
 
 create table role (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
@@ -18,7 +18,7 @@ references department (id)
 );
 
 create table employee (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30), 
 last_name VARCHAR(30),
 role_id INT,
---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (title)
+VALUES
+    ("Marketing"),
+    ("Information Technology"),
+    ("Software Development"),
+    ("Human Resources"),
+    ("Accounting");
+    
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Marketing Coordinator", 50000, 1),
+    ("IT Analyst", 70000, 2),
+    ("Full-Stack Developer", 80000, 3),
+    ("Human Resources Director", 100000, 4),
+    ("Accounting Coordinator", 50000, 5),
+    ("IT Director", 120000, 6),
+    ("Accounting Director", 100000, 7)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("John", "Doe", 4, NULL),
+    ("Rachel", "Martin", 6, NULL),
+    ("Mark", "Adams", 7, NULL),
+    ("Brad", "Jones", 1, 4),
+    ("Jessica", "Smith", 2, 6),
+    ("Jake", "Miller", 3, 6),
+    ("Albert", "Jones", 5, 7)
