de914c49eb8cb8a3717d83acad5f68e73f0ba587
allenmurfee <allenmurfee123@gmail.com>
2022-11-08 16:00:00 -0500

    seeds.sql file working

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-delete database if exists company_db;
+drop database if exists company_db;
 create database company_db;
 
 use company_db;
 
-create table department (
+CREATE TABLE department (
 id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30) 
 );
 
-create table role (
+CREATE TABLE role (
 id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-foreign key (department_id) references department (id) ON DELETE CASCADE
+FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
-create table employee (
+CREATE TABLE employee (
 id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30), 
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-foreign key (role_id) references role(id) ON DELETE CASCADE,
-foreign key (manager_id) references employee(id) ON DELETE CASCADE
+FOREIGN KEY (role_id) REFERENCES ROLE(id) ON DELETE CASCADE,
+FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
