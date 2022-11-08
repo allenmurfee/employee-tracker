98342f7e51b6dbb52d7c44996d2205c5e160be0e
allenmurfee <allenmurfee123@gmail.com>
2022-11-08 15:36:11 -0500

    working on sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,24 +4,27 @@ VALUES
     ("Information Technology"),
     ("Software Development"),
     ("Human Resources"),
-    ("Accounting");
+    ("Accounting"),
+    ("Administration");
     
 INSERT INTO role (title, salary, department_id)
 VALUES
+    ("CEO", 200000, 6),
+    ("IT Director", 120000, 2),
+    ("Accounting Director", 100000, 5)
+    ("Human Resources Director", 100000, 4),
     ("Marketing Coordinator", 50000, 1),
     ("IT Analyst", 70000, 2),
-    ("Full-Stack Developer", 80000, 3),
-    ("Human Resources Director", 100000, 4),
-    ("Accounting Coordinator", 50000, 5),
-    ("IT Director", 120000, 6),
-    ("Accounting Director", 100000, 7);
+    ("Full Stack Developer", 80000, 3),
+    ("Accounting Coordinator", 50000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("John", "Doe", 4, NULL),
-    ("Rachel", "Martin", 6, NULL),
-    ("Mark", "Adams", 7, NULL),
-    ("Brad", "Jones", 1, 4),
-    ("Jessica", "Smith", 2, 6),
-    ("Jake", "Miller", 3, 6),
-    ("Albert", "Jones", 5, 7);
+    ("Jack", "Murphy", 1, NULL),
+    ("John", "Doe", 2, 1),
+    ("Rachel", "Martin", 3, 1),
+    ("Jessica", "Smith", 4, 1),
+    ("Mark", "Adams", 5, 4),
+    ("Brad", "Jones", 6, 2),
+    ("Jake", "Miller", 7, 2),
+    ("Albert", "Jones", 8, 3);
